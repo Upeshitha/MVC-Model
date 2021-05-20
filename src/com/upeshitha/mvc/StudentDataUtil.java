a3f3a202a0dedaa825281dac944d4309cfcdb016
Eranda Upeshitha <upeshitha@gmail.com>
2021-05-20 20:58:57 +0530

    add mvc model

---
+++ src/com/upeshitha/mvc/StudentDataUtil.java
@@ -0,0 +1,24 @@
+package com.upeshitha.mvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class describes the students details
+ * @version   20 May 2021
+ * @author    Eranda Upeshitha
+ */
+public class StudentDataUtil {
+	
+	public static List<Student> getStudents() {
+		
+		List<Student> studentsList = new ArrayList<>();
+		studentsList.add(new Student("Eranda", "Upeshitha", "upeshitha@email.com"));
+		studentsList.add(new Student("Kasun", "Kumara", "kasun@email.com"));
+		studentsList.add(new Student("Shamal", "silwa", "shamal@email.com"));
+		studentsList.add(new Student("Kavidu", "Sirimal", "kavidu@email.com"));
+		
+		return studentsList;
+	}
+
+}
